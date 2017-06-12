8a37d7dcac0dd091bab4eae756a20a57dda34518
Devin Shoemaker <devinshoe@gmail.com>
2017-06-11 21:29:11 -0500

    TasksProvider: Task CRUD

--- src/providers/tasks/tasks.ts
+++ src/providers/tasks/tasks.ts
@@ -6,6 +6,7 @@ import PouchDB from 'pouchdb';
 @Injectable()
 export class TasksProvider {
 
+  data: any;
   db: any;
   remote: any;
 
@@ -21,6 +22,73 @@ export class TasksProvider {
     };
 
     this.db.sync(this.remote, options);
+  }
+
+  getAllTasks() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.db.allDocs({
+        include_docs: true
+      }).then((result) => {
+        this.data = [];
+
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({live: true, since: 'now', include_docs:true}).on('change', (change) => {
+          this.handleChange(change);
+        });
+      }).catch((error) => {
+        console.log(error);
+      })
+    })
+  }
+
+  createTask(task) {
+    this.db.post(task);
+  }
+
+  updateTask(task) {
+    this.db.put(task).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  deleteTask(task) {
+    this.db.remove(task).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  handleChange(change) {
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+    });
+
+    // A document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
+    } else {
+      // A document was updated or added
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      } else {
+        this.data.push(change.doc);
+      }
+
+    }
   }
 
 }
