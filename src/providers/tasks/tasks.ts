9699e4ab6240aac0d77ef1646b36b007f691c3a9
Devin Shoemaker <devinshoe@gmail.com>
2017-06-11 20:02:39 -0500

    Create a new PouchDB database `taskit`

--- src/providers/tasks/tasks.ts
+++ src/providers/tasks/tasks.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import PouchDB from 'pouchdb';
 
-/*
-  Generated class for the TasksProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class TasksProvider {
 
+  db: any;
+
   constructor(public http: Http) {
-    console.log('Hello TasksProvider Provider');
+    this.db = new PouchDB('taskit');
   }
 
 }
