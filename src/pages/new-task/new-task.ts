1e326443149fbe590f99ee9e879350348231ef8d
Devin Shoemaker <devinshoe@gmail.com>
2017-06-12 22:04:36 -0500

    NewTaskPage: disable FAB until title is entered

--- src/pages/new-task/new-task.ts
+++ src/pages/new-task/new-task.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {TasksProvider} from "../../providers/tasks/tasks";
+import { TasksProvider } from "../../providers/tasks/tasks";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 /**
  * Save new tasks.
@@ -12,13 +13,20 @@ import {TasksProvider} from "../../providers/tasks/tasks";
 })
 export class NewTaskPage {
 
-  task: { title: string } = {
-    title: ''
-  };
+  taskForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public taskService: TasksProvider) {}
+  task: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public taskService: TasksProvider, public formBuilder: FormBuilder) {
+    this.taskForm = formBuilder.group({
+      title: ['', Validators.required]
+    });
+  }
 
   createTask() {
+    this.task = {
+      title: this.taskForm.controls['title'].value
+    };
     this.taskService.createTask(this.task);
     this.navCtrl.pop();
   }
